d5e305fd693cf75439472aa6532be425f55a73e3
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-03-09 17:03:56 +0700

    chore: init

---
+++ week-1/pbi-core-initiative/src/components/HomeComponent.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {},
+  data() {}
+}
+</script>
+
+<template>
+  <p>Anime kan bang</p>
+</template>
---
+++ week-1/pbi-core-initiative/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'HomeComponent',
+  props: { name: { type: String } },
+  data() {
+    return {
+      drinks: [
+        { name: 'Anime kan bang', price: '$3.00' },
+        { name: 'Soda', price: '$2.50' }
+      ]
+    }
+  }
+}
+</script>
+<template>
+  <p>{{ name }}</p>
+</template>
